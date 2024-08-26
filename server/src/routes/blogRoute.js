63f78a7091f2ace032b510d181c8a8487251f927
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-08-26 19:10:57 +0100

    router for CRUD oeration

--- server/src/routes/blogRoute.js
+++ server/src/routes/blogRoute.js
@@ -1,3 +1,16 @@
 const express = require('express')
 
+const { getAllBlogs, deleteBlog, updateBlog, createBlog, getSingleBlog } = require('../controller/blogController')
+
 const blogRouter = express.Router()
+
+
+blogRouter.use('/').get(getAllBlogs).post(createBlog)
+
+blogRouter.use('/:id').put(updateBlog).delete(deleteBlog).get(getSingleBlog)
+
+blogRouter.use('*', (req, res, ) => {
+    res.send('router not found')
+})
+
+module.exports = blogRouter
