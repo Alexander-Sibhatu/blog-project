5d9882eb8590bfa9ab76ee7719a5029b3cff07f8
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-11-09 17:01:32 +0000

    cookie parser and user route added

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const createError = require('http-errors')
 const dev = require('./config')
 const connectDB = require('./config/db')
 const blogRouter = require('./routes/blogRoute')
+const userRouter = require('./routes/userRoute')
 
 const app = express() 
 
@@ -18,11 +20,15 @@ const port = dev.app.serverPort
 app.use(cors({
     origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
     credentials: true,
-}))
+    })
+)
+app.use(cookieParser())
+app.use('/public', express.static('public'))
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 app.use((req, res, next) => {
     next(createError(404, 'Route is not found'))
