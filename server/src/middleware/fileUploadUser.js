ce5afe14d7a32ca5a834bad20545b17555380381
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-11-10 00:53:13 +0000

    multer uploader for user created

---
+++ server/src/middleware/fileUploadUser.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+const path = require('path');
+
+const FILE_SIZE = 1024 * 1024 * 2; // Limit file size to 2MB for user profiles
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname, '../public/images/users')); // Set user-specific upload folder
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+});
+
+const uploadUser = multer({ storage: storage, limits: { fileSize: FILE_SIZE }});
+
+module.exports = uploadUser;
