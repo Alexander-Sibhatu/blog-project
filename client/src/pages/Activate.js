c5520f8e5bdfdac10591dbfba5d9f549aeab06f9
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-11-20 20:29:59 +0000

    activation component done

--- client/src/pages/Activate.js
+++ client/src/pages/Activate.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { activateUser } from '../services/UserServices'
+import { toast } from 'react-toastify'
 
 const Activate = () => {
+
+  const { token } = useParams();
+  const navigate = useNavigate();
+
+  const handleActivateUser = async () => {
+    try {
+      await activateUser( token );
+      toast.success("Account activated successfully!")
+      navigate("/login") // Redirect on success
+    } catch (error) {
+      console.log(error)
+      // toast.error(error.response.data.error.message)
+    }
+  }
   return (
-    <div>Activate</div>
+    <div>
+      <button onClick={handleActivateUser}>Activate User</button>
+    </div>
   )
 }
 
