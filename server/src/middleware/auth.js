5fa802c408e8522dd451f40d074c8f87e17084c2
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-10 19:07:28 +0000

    middlewared for loggedin loggedout isadmin

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,64 @@
+const createError = require('http-errors');
+const dev = require('../config');
+const User = require('../models/userModel');
+
+const isLoggedIn = (req, res, next) => {
+    try {
+        // cookies here
+        if(!res.headers.cookie) {
+            return res.status(404).send({
+                message: 'No cookie found',
+            })
+        }
+
+        const token = req.headers.cookie.split('=')[1];
+
+        // verify token
+        if (!token){
+            return res.status(404).send({
+                message: 'No token found',
+            })
+        }
+
+        const decoded = jwt.verify(res, token, String(dev.app.jwtAuthorizationSecretKey));
+        if (!decoded) throw createError(403, 'Invalid Token');
+        req._id = decoded._id;
+        next();
+    } catch (error) {
+        next(error)
+    }
+}
+
+const isLoggedOut = (req, res, next) => {
+    try {
+        if(req.headers.cookie) {
+            throw createError(
+                401,
+                'Not accessible request, you need to be logged in'
+            )
+        }
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+const isAdmin = async (req, res, next ) => {
+    try {
+        const id = req._id;
+        if(id) {
+            const user = await User.findById(id);
+            if(!user) throw createError(404, 'No user found with this id');
+
+            // check if user is admin
+            if (!user.is_admin) throw createError(401, 'User is not an admin');
+
+            next();
+        } else {
+            throw createError(400, 'Please login')
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+module.exports = { isLoggedIn, isLoggedOut, isAdmin}
